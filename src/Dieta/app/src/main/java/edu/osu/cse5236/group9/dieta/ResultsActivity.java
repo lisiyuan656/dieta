505713255458d33631f428cae06dcf86ed35d431
Jiyuan <zjtxljyus@gmail.com>
2016-10-16 17:38:04 -0400

    Missing Files Added

--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/ResultsActivity.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/ResultsActivity.java
@@ -5,41 +5,38 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 
-public class DetailedResultsActivity extends FragmentActivity implements View.OnClickListener{
+public class ResultsActivity extends FragmentActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed_results);
+        setContentView(R.layout.activity_results);
 
         ResultsFragment resultsFragment= new ResultsFragment();
         FragmentManager fragmentManager=getSupportFragmentManager();
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
 
-        fragmentTransaction.add(R.id.detailedresults_nfacts,resultsFragment);
+        fragmentTransaction.add(R.id.results_nfacts,resultsFragment);
         fragmentTransaction.commit();
 
-        View buttonLeft=findViewById(R.id.detailedresults_left);
-        View buttonRight=findViewById(R.id.detailedresults_right);
-        View buttonFinish=findViewById(R.id.detailedresults_finish);
-        buttonLeft.setOnClickListener(this);
-        buttonRight.setOnClickListener(this);
+        View buttonDetails=findViewById(R.id.results_detail);
+        View buttonFinish=findViewById(R.id.results_finish);
+        buttonDetails.setOnClickListener(this);
         buttonFinish.setOnClickListener(this);
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.detailedresults_left:
-                // TODO: left
+            case R.id.results_detail:
+                startActivity(new Intent(this,DetailedResultsActivity.class));
                 break;
-            case R.id.detailedresults_right:
-                // TODO: right
-                break;
-            case R.id.detailedresults_finish:
+            case R.id.results_finish:
                 startActivity(new Intent(this,NewFoodActivity.class));
                 break;
         }
     }
+
 }
