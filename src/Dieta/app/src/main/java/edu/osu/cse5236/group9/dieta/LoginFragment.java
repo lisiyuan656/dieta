6f4fe3e608dec13b64c69ff65b3675fbf75496d2
Siyuan <lisiyuan656@gmail.com>
2016-10-16 16:39:09 -0400

    Add LoginActivity.java, LoginFragment.java

--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginFragment.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginFragment.java
@@ -1,10 +1,13 @@
 package edu.osu.cse5236.group9.dieta;
 
-/**
- * Created by Siyuan on 10/9/16.
- */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class LoginFragment {
+public class LoginFragment extends Fragment {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +16,7 @@ public class LoginFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_new_food, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        return v;
+    }
 }
