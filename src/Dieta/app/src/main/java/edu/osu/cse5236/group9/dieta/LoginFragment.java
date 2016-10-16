b9d108dd8ff5561880cc3a13c4a79d0b26c74eb0
Siyuan <lisiyuan656@gmail.com>
2016-10-16 16:16:43 -0400

    Fix bugs, add LoginFragment.java

--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginFragment.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginFragment.java
@@ -5,4 +5,13 @@ package edu.osu.cse5236.group9.dieta;
  */
 
 public class LoginFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_new_food, container, false);
 }
