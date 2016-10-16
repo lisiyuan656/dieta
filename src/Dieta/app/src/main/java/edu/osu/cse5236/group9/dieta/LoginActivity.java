6ecc653e79055ddf44c6df0bacdf2f420fbbbf5b
Siyuan <lisiyuan656@gmail.com>
2016-10-16 17:43:36 -0400

    Add Button to LoginFragment

--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginActivity.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginActivity.java
@@ -3,7 +3,6 @@ package edu.osu.cse5236.group9.dieta;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends FragmentActivity {
@@ -16,7 +15,7 @@ public class LoginActivity extends FragmentActivity {
         FragmentManager fm = getSupportFragmentManager();
         Fragment login_fragment = fm.findFragmentById(R.id.login_fragment_container);
         if (login_fragment==null) {
-            login_fragment = new NewFoodFragment();
+            login_fragment = new LoginFragment();
             fm.beginTransaction()
                     .add(R.id.login_fragment_container, login_fragment)
                     .commit();
--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginFragment.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginFragment.java
@@ -5,18 +5,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginFragment extends Fragment {
+    private EditText mEditText_Username;
+    private EditText mEditText_Password;
+    private Button mButton_Login;
+    private Button mButton_Exit;
+    private Button mButton_NewUser;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    // TODO: Input Handlers
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_login, container, false);
+        mButton_Login = (Button) v.findViewById(R.id.login_login);
+        mButton_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: authentication
+            }
+        });
+        mButton_Exit = (Button) v.findViewById(R.id.login_exit);
+        mButton_Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: save before exit
+                // TODO: Exit the activity
+            }
+        });
+        mButton_NewUser = (Button) v.findViewById(R.id.login_newuser);
+        mButton_NewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: jump to NewUserActivity
+            }
+        });
         return v;
     }
 }
