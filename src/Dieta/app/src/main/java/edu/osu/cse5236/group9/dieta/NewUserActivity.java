a935aad2bee7f88e18c9ab862de3af55d990d544
Siyuan <lisiyuan656@gmail.com>
2016-10-17 19:42:31 -0400

    Finish NewUser Activity

--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/NewUserActivity.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/NewUserActivity.java
@@ -2,12 +2,24 @@ package edu.osu.cse5236.group9.dieta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class NewUserActivity extends Activity {
+public class NewUserActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment newuser_fragment = fm.findFragmentById(R.id.new_user_fragment_container);
+        if (newuser_fragment==null) {
+            newuser_fragment = new NewUserFragment();
+            fm.beginTransaction()
+                    .add(R.id.new_user_fragment_container, newuser_fragment)
+                    .commit();
+        }
     }
 }
