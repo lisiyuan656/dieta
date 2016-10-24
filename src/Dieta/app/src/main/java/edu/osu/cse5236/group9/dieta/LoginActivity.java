d0bcefefdb3ad78aadf1afc29e6c73b3a6870d2c
Siyuan <lisiyuan656@gmail.com>
2016-10-23 23:52:53 -0400

    Add breakpoint to checkpoint3

--- src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginActivity.java
+++ src/Dieta/app/src/main/java/edu/osu/cse5236/group9/dieta/LoginActivity.java
@@ -8,12 +8,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends SingleFragmentActivity {
+public class LoginActivity extends FragmentActivity {
     private Button mButton_Exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(this.getClass().getSimpleName(), "onCreate(Bundle) called");
+        setContentView(R.layout.activity_login);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.login_fragment_container);
+        if (fragment==null) {
+            fragment = new LoginFragment();
+            fragmentManager.beginTransaction().add(R.id.login_fragment_container, fragment).commit();
+        }
         mButton_Exit = (Button) findViewById(R.id.login_exit);
         mButton_Exit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,7 +34,32 @@ public class LoginActivity extends SingleFragmentActivity {
     }
 
     @Override
-    protected Fragment createFragment() {
-        return new LoginFragment();
+    public void onStart() {
+        super.onStart();
+        Log.d(this.getClass().getSimpleName(), "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(this.getClass().getSimpleName(), "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(this.getClass().getSimpleName(), "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(this.getClass().getSimpleName(), "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(this.getClass().getSimpleName(), "onDestroy() called");
     }
 }
